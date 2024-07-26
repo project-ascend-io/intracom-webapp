cf1ac9649654e7dab5434800e7c243d0b7d5ce3c
Oscar Estrada <16450128+oestrada1001@users.noreply.github.com>
2024-07-26 15:49:58 -0700

    feat(accept page): add accept/deny page

--- src/app/(public)/user-invites/[hash]/page.tsx
+++ src/app/(public)/user-invites/[hash]/page.tsx
@@ -11,16 +11,131 @@ type UserInviteForm = {
 type Invitation = {
     _id: string;
     email: string;
-    organization: string;
+    organization: {
+        _id: string;
+        name: string;
+    };
     state: string;
     hash: string;
 }
+
 export default function ViewUserInvitePage() {
-    const router = useRouter();
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [email, setEmail] = useState<string>('foo@bar.com');
-;
+    const dummyInvite: Invitation = {
+        _id: "random-hash",
+        email: "foo@bar.com",
+        organization: {
+            _id: "random-org-id",
+            name: "Letstat's Coffee Shop"
+        },
+        state: "pending",
+        hash: "random-invite-hash"
     }
+
+    const router = useRouter();
+    const [invite, setInvite] = useState<Invitation>(dummyInvite);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const submitInviteResponse = (hasAccepted: boolean) => {
+        console.log("Responding to invite...");
+        setIsLoading(true);
+
+        if (hasAccepted) {
+            setTimeout(() => {
+                const newInvite = {
+                    ...invite,
+                    state: "accepted"
+                };
+                setInvite(newInvite);
+                setIsLoading(false);
+            }, 3000);
+        } else {
+            setTimeout(() => {
+                const newInvite = {
+                    ...invite,
+                    state: "denied"
+                };
+                setInvite(newInvite);
+                setIsLoading(false);
+            }, 3000);
+        }
+    }
+
+    const feedback = () => {
+
+    }
+
+    if (invite?.state == "denied") {
+        return (
+          <>
+              <div className="container mx-auto md:w-9/12">
+                  <div className="text-center mb-12 md:mb-6">
+                      <h1 className="mt-6 mb-2 text-xl font-bold">Thank you updating the invitation.</h1>
+                      <p>Before completing your request, can you provide feedback on why you&apos;ve denied the invitation?</p>
+                  </div>
+                  <form action={feedback}>
+                      <div className="my-2">
+                          <label className="flex items-center">
+                              <input type="radio" name="reason" value="organization" className="radio mx-3" />
+                              <span>I don&apos;t know the organization.</span>
+                          </label>
+                      </div>
+                      <div className="my-2">
+                          <label className="flex items-center">
+                              <input type="radio" name="reason" value="interest" className="radio mx-3" />
+                              <span>I&apos;m not interested joining Intacom.</span>
+                          </label>
+                      </div>
+                      <div className="my-2">
+                          <label className="flex items-center">
+                              <input type="radio" name="reason" value="other" className="radio mx-3" />
+                              <span>Other</span>
+                          </label>
+                      </div>
+                      <div className="my-4">
+                          <button type="submit"
+                                  className="btn btn-error rounded-lg text-white"
+                                  disabled={isLoading}>
+                              Deny Invitation
+                          </button>
+                      </div>
+                  </form>
+              </div>
+          </>
+        );
+    }
+
+    if (invite?.state == "pending") {
+        return (
+          <>
+              <div className="container mx-auto md:w-9/12">
+                  <div className="text-center mb-12 md:mb-6">
+                      <h1 className="mt-6 mb-2 text-xl font-bold">You&apos;ve been invited to
+                          join {invite?.organization.name} on Intracom</h1>
+                  </div>
+                  <div className="my-3 w-full md:w-96 mx-auto">
+                      <button type="submit"
+                              onClick={() => submitInviteResponse(true)}
+                              className="btn btn-primary rounded-lg text-white w-full"
+                              disabled={isLoading}>
+                          Accept
+                      </button>
+                  </div>
+                  <div className="text-center">
+                      <p>Or</p>
+                  </div>
+                  <div className="my-3 w-full md:w-96 mx-auto">
+                      <button type="submit"
+                              onClick={() => submitInviteResponse(false)}
+                              className="btn btn-secondary rounded-lg text-white w-full"
+                              disabled={isLoading}>
+                          No, thanks.
+                      </button>
+                  </div>
+              </div>
+          </>
+        );
+    }
+
     /*
     1. Get hash from URL
     2. GET request to back to verify invitation is valid (not expired or deleted)  -> return inviation
@@ -31,7 +146,7 @@ export default function ViewUserInvitePage() {
         setIsLoading(true);
 
         const username = formData.get("username");
-        const email = formData.get("email");
+        const email = invite?.email;
         const password = formData.get("password");
         const terms = formData.get("terms");
 
@@ -66,7 +181,7 @@ export default function ViewUserInvitePage() {
                                    className="input input-bordered w-full max-w-lg rounded-lg"/>
                         </div>
                         <div className="my-3">
-                            <input type="email" name="email" placeholder="Your email" value={email} disabled={true}
+                            <input type="email" name="email" placeholder="Your email" value={invite?.email} disabled={true}
                                    className="input input-bordered w-full max-w-lg rounded-lg"/>
                         </div>
                         <div className="my-3">
