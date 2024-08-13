3d2d1ab041d8ee5bd08b814bf47061c4e2e97694
tolobayo <tolobayo1@gmail.com>
2024-08-13 15:32:24 -0400

    fix(users page): remove tr element causing hydration error

--- src/app/(public)/users/page.tsx
+++ src/app/(public)/users/page.tsx
@@ -1,55 +1,51 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
 
 export default function ViewUserListPage() {
-    const [userData, setData] = useState();
+  const [userData, setData] = useState();
 
-    
-    useEffect(() => {
-        fetch('https://dummyjson.com/users')
-            .then(res => res.json())
-            .then(res => setData(res.users));
-    
-    },[])
-  
-    return (
-        <>
-            <div>
-                <p>Users</p>
-                <h1>Search Users</h1>
-            </div>
-            <div>
-               
-            </div>
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((res) => setData(res.users));
+  }, []);
 
-            <table>
-                <thead>
+  return (
+    <>
+      <div>
+        <p>Users</p>
+        <h1>Search Users</h1>
+      </div>
+      <div></div>
 
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Gender</th>
-                        <th>Date of Birth</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {userData && userData.map((data) => {
-                        return (
-                            <tr>
-                                <td>{data.firstName} {data.lastName}</td>
-                                <td>{data.email}</td>
-                                <td>{data.gender}</td>
-                                <td>{data.birthDate}</td>
-                                <td>View</td>
-                            </tr>
-                        )
-                    })}
-
-                </tbody>
-                <tr></tr>
-            </table>
-        </>
-    )
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Date of Birth</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userData &&
+            userData.map((data) => {
+              return (
+                <tr>
+                  <td>
+                    {data.firstName} {data.lastName}
+                  </td>
+                  <td>{data.email}</td>
+                  <td>{data.gender}</td>
+                  <td>{data.birthDate}</td>
+                  <td>View</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </>
+  );
 }
