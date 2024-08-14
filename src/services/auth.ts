23c45d9cdc02053f702fc6d096dbf1ad2bde5f23
tolobayo <tolobayo1@gmail.com>
2024-08-14 11:34:24 -0400

    feat(auth): complete check and logout functionality

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -19,9 +19,49 @@ export async function login(formData: LoginFormType) {
   const data = await fetchFromAPI(fetchParams);
 
   if (data.success)
-    localStorage.setItem("user", JSON.stringify(data.responseObject));
+    localStorage.setItem("intracom-user", JSON.stringify(data.responseObject));
 
   return data;
 }
-export async function checkUser() {}
-export async function logout() {}
+
+/**
+ * Check user session
+ * @returns {object} - response data or error object returned from the server
+ */
+export async function checkSession() {
+  const fetchParams = {
+    method: "GET",
+    endpoint: "/auth/check",
+    errorMessage: "Failed to validate user session",
+  };
+
+  console.log("check FETCH Params", fetchParams);
+
+  const data = await fetchFromAPI(fetchParams);
+
+  if (data.success)
+    localStorage.setItem("intracom-user", JSON.stringify(data.responseObject));
+  else localStorage.removeItem("intracom-user");
+
+  return data;
+}
+
+/**
+ * Logout user
+ * @returns {ResponseObject} The response from the server
+ */
+export async function logout() {
+  const fetchParams = {
+    method: "POST",
+    endpoint: "/auth/logout",
+    errorMessage: "Failed to logout user",
+  };
+
+  console.log("logout FETCH Params", fetchParams);
+
+  const data = await fetchFromAPI(fetchParams);
+
+  if (data.success) localStorage.removeItem("intracom-user");
+
+  return data;
+}
