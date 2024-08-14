0784c545560d19c7a777d9e36a4b1b6fd56880cd
tolobayo <tolobayo1@gmail.com>
2024-08-14 15:14:59 -0400

    feat(auth): implement route protection within auth layer

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,20 +1,40 @@
-import { ReactNode } from "react";
-import { AuthProvider } from "@/context/auth";
+"use client";
+import { ReactNode, useEffect, useState } from "react";
+import { checkSession } from "@/services/auth";
+import { useAuth } from "@/context/auth";
+import { redirect } from "next/navigation";
 
 type Props = {
   children: ReactNode;
 };
 export default function AuthLayout({ children }: Props) {
-  return (
-    <AuthProvider>
-      <div className="w-full">
-        <div className="w-full border-b-2 border-solid">
-          <div className="container md:mx-auto navbar">
-            <a className="btn btn-ghost text-xl">Intracom</a>
-          </div>
+  const [loading, setLoading] = useState(true);
+  const { user, setUser } = useAuth();
+  console.log(!user);
+  async function checkUserSession() {
+    const data = await checkSession();
+    if (data?.success) {
+      setUser(data.responseObject);
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    if (!user) checkUserSession();
+    else setLoading(false);
+  }, []);
+
+  if (loading) return;
+
+  return user ? (
+    <div className="w-full">
+      <div className="w-full border-b-2 border-solid">
+        <div className="container md:mx-auto navbar">
+          <a className="btn btn-ghost text-xl">Intracom</a>
         </div>
-        <main className="container px-4 md:px-0 mx-auto">{children}</main>
       </div>
-    </AuthProvider>
+      <main className="container px-4 md:px-0 mx-auto">{children}</main>
+    </div>
+  ) : (
+    redirect("/")
   );
 }
--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -5,7 +5,17 @@ import { AuthContextType, AuthUserType, AuthProviderType } from "../types/auth";
 const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider({ children }: AuthProviderType) {
-  const [user, setUser] = useState<AuthUserType | null>(null);
+  const userObject = JSON.parse(localStorage.getItem("user") || "");
+
+  const [user, setUser] = useState<AuthUserType | null>(userObject || null);
+
+  const expires = userObject?.expires;
+
+  // if current date surpases the expire date clear user and local storage
+  if (expires && Date.parse(expires) < Date.parse(new Date().toISOString())) {
+    setUser(null);
+    localStorage.removeItem("user");
+  }
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
