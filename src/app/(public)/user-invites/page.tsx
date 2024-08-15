b5eb46a21778549f1b1e3c5161c8bf8c385e1e25
Amenu-sh <amanushifara@gmail.com>
2024-08-15 09:49:22 -0500

    refactor(remove): Removed double folders

---
+++ src/app/(public)/user-invites/page.tsx
@@ -0,0 +1,7 @@
+export default function UserInvitePage() {
+    return (
+        <>
+            <h1>User Invite Page</h1>
+        </>
+    )
+};
---
+++ src/hooks/use-invites.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { ApiResponseWrapper, Invitation, UserInviteParams } from "@/app/(public)/user-invites/[hash]/types";
+const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+
+export const useInvite = ({ hash }: UserInviteParams) => {
+  const [invite, setInvite] = useState<Invitation | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<ApiResponseWrapper | null>(null);
+
+  useEffect(() => {
+    const fetchInvite = async () => {
+      try {
+        const response = await fetch(API_URL + '/user-invites/' + hash, {
+            cache: 'no-store'
+        });
+
+        if (!response.ok) {
+          const data = await response.json();
+          setError(data);
+          setIsLoading(false);
+          return;
+        }
+
+        const data = await response.json();
+        const userInvitation = data.responseObject;
+        setInvite(userInvitation);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err);
+        setIsLoading(false);
+      }
+    };
+    fetchInvite();
+  }, []);
+
+  return { invite, isLoading, error, setInvite, setIsLoading };
+};
