82e8ce6852f2163695f46d0707403b392685d135
Amenu-sh <amanushifara@gmail.com>
2024-08-16 00:03:11 -0500

    updates

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,26 @@
 /* eslint-env node */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'revert',
+      ],
+    ],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+  },
 };
+
