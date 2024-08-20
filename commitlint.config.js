89bcdf48897190b94a63b97c0b562e38741e7a2d
Oscar Estrada <16450128+oestrada1001@users.noreply.github.com>
2024-08-19 19:47:39 -0700

    testing commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,5 @@
-/* eslint-env node */
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'chore',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'revert',
-      ],
-    ],
-    'subject-empty': [2, 'never'],
-    'type-empty': [2, 'never'],
-  },
+export default { 
+    extends: [
+        '@commitlint/config-conventional'
+    ] 
 };
