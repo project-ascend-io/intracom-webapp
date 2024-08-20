a94329a8898f57c239549ec63445ea5840a4d3f9
Oscar Estrada <16450128+oestrada1001@users.noreply.github.com>
2024-08-19 19:42:51 -0700

    Revert "no standard"
    This reverts commit 75c0b1149115c62165c39825282ddf6293f1fbdd.

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
