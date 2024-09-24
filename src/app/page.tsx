c01d1d1329fb898bc062bb9141698a65cfe7f596
Oscar Estrada <16450128+oestrada1001@users.noreply.github.com>
2024-09-24 13:54:17 -0700

    style(src/page.tsx): remove empty space and run npm run lint:fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ const HomePage = () => {
     <div className='flex h-screen items-center justify-center bg-black text-white'>
       <div className='text-center'>
         <div className=''>
-          <h1 className='bg-gradient-to-r from-green-400 via-purple-500 to-pink-500 bg-clip-text text-6xl font-bold text-transparent py-2'>
+          <h1 className='bg-gradient-to-r from-green-400 via-purple-500 to-pink-500 bg-clip-text py-2 text-6xl font-bold text-transparent'>
             Coming Soon
           </h1>
           <p className='mx-auto p-5 text-center text-lg lg:w-[70%]'>
@@ -21,8 +21,6 @@ const HomePage = () => {
             be economically accessible and customizable for startups,
             nonprofits, and small companies.
           </p>
-
-          
         </div>
       </div>
     </div>
