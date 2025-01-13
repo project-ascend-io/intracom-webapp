cb155afdf9d87faeb1822b49ecc79c0b99077811
Oscar Estrada <16450128+oestrada1001@users.noreply.github.com>
2025-01-13 15:10:03 -0800

    feat(PAP-242): update footer links

--- src/page-components/FooterSection.tsx
+++ src/page-components/FooterSection.tsx
@@ -11,16 +11,22 @@ const FooterSection = () => {
         </aside>
         <nav className="text-white">
           <h6 className='footer-title'>Documentation</h6>
-          <a className='link-hover link'>Github</a>
+          <a href="https://github.com/project-ascend-io/intracom-electron"
+            target='_blank'
+            className='link-hover link'>Github</a>
           <a className='link-hover link'>Slack Channel</a>
-          <a className='link-hover link'>Project Management</a>
-          <a className='link-hover link'>Product Requirement Document</a>
+          <a href="https://project-ascend-io.atlassian.net/jira/software/c/projects/PAP/boards/1"
+            target='_blank'
+            className='link-hover link'>Project Management</a>
+          <a href="https://guide.projectascend.io/intracom"
+            target='_blank'
+            className='link-hover link'>Product Requirement Document</a>
         </nav >
         <nav className="text-white">
           <h6 className='footer-title'>Intracom</h6>
-          <a className='link-hover link'>About</a>
-          <a className='link-hover link'>Community</a>
-          <a className='link-hover link'>Contributors</a>
+          <a href="/" className='link-hover link'>About</a>
+          <a href="/#community" className='link-hover link'>Community</a>
+          <a href="/#contributors" className='link-hover link'>Contributors</a>
           <a className='link-hover link'>Press kit</a>
         </nav>
         <nav className="text-white">
